b84262e574081c4493a15c5e52e124ef65407d6e
Bain Hansly Cruz <repatobain@gmail.com>
2025-05-08 04:46:55 +0800

    hide forgot password for feature implementation

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -119,12 +119,12 @@ export function LoginForm({
               <FormItem>
                 <div className="flex items-center">
                   <FormLabel>Password</FormLabel>
-                  <a
+                  {/* <a
                     href="/forgot-password"
                     className="ml-auto text-sm underline-offset-4 hover:underline"
                   >
                     Forgot your password?
-                  </a>
+                  </a> */}
                 </div>
                 <FormControl>
                   <Input type="password" disabled={isLoading} {...field} />
